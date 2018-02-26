d86803e19d1540ca2b3c4ac4e41169369c9e3fdf
R0vi <ivorstaatsl0l@gmail.com>
2018-02-26 10:08:47 +0100

    chuckaluck

---
+++ Week2Les7chuckaluck/src/Dobbelsteen.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+class Dobbelsteen {
+    public int waarde;
+
+    Dobbelsteen() {
+        Random rand = new Random();
+        waarde = rand.nextInt(6)+1;
+    }
+
+    public String toString() {
+        return("De gedobbelde waarde is" + waarde);
+    }
+}
